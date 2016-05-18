88a89122ccc83e9395500b5868243491997085f4
Oliver <s57588@beuth-hochschule.de>
2016-05-18 16:39:30 +0200

    Man macht das Spaß

--- js/eintrag.js
+++ js/eintrag.js
@@ -1,66 +1,51 @@
 
-// Die Validierung der Einträge
-function checkEntries() {
-    
-    var form = document.forms["eintrag"],
-        name = document.getElementById("name"),
-        place = document.getElementById("place"),
-        email = document.getElementById("email");
+var form = document.forms[0],
+ submit = document.getElementById("submit"),
+ nameInput = document.getElementById("name");
+ countryInput = document.getElementById("country");
+ emailInput = document.getElementById("email");
+ timefromInput = document.getElementById("timefrom");
+ timetoInput = document.getElementById("timeto");
 
-    // Name des Ortes
-    if (name.equals("")) {
+nameInput.addEventListener("invalid", function(e) {
+ if(nameInput.validity.valueMissing){
+ e.target.setCustomValidity("Bitte den Namen des Drehortes angeben!");
+ } else if(!nameInput.validity.valid) {
+ e.target.setCustomValidity("Dies ist keine gültige Angabe!");
+ }
+}, false);
 
-        alert("Das Feld darf nicht leer sein!");
+countryInput.addEventListener("invalid", function(e) {
+ if(countryInput.validity.valueMissing){
+ e.target.setCustomValidity("Bitte den Namen des Ortes angeben!");
+ } else if(!countryInput.validity.valid) {
+ e.target.setCustomValidity("Dies ist keine gültige Angabe!");
+ }
+}, false);
 
-    }
+emailInput.addEventListener("invalid", function(e) {
+ if(emailInput.validity.valueMissing){
+ e.target.setCustomValidity("Bitte die Email-Adresse des Veranstalters angeben!");
+ } else if(!emailInput.validity.valid) {
+ e.target.setCustomValidity("Dies ist keine gültige Angabe!");
+ }
+}, false);
 
-    // Wo der Irt ist
+timefromInput.addEventListener("invalid", function(e) {
+ if(timefromInput.validity.valueMissing){
+ e.target.setCustomValidity("Bitte die Uhrzeit der Veranstaltung angeben!");
+ } else if(!timefromInput.validity.valid) {
+ e.target.setCustomValidity("Dies ist keine gültige Angabe!");
+ }
+}, false);
+
+timetoInput.addEventListener("invalid", function(e) {
+ if(timetoInput.validity.valueMissing){
+ e.target.setCustomValidity("Bitte die Uhrzeit der Veranstaltung angeben!");
+ } else if(!timetoInput.validity.valid) {
+ e.target.setCustomValidity("Dies ist keine gültige Angabe!");
+ }
+}, false);
 
 
-    // Email Addresse
-
-    if (email.indexOf("@") < 0) {
-        alert("Keine Emailadresse");
-
-
-    }
-
-    // Tage
-
-
-    // Uhrzeit
-
-}
-
-
-
-//nameInput.addEventListener("invalid", function(e) {
-// if(nameInput.validity.valueMissing){
-// e.target.setCustomValidity("Please create a username");
-// } else if(!nameInput.validity.valid) {
-// e.target.setCustomValidity("This is not a valid username");
-// }
-// // avoid the problem when resuming typing after getting a custom invalid message
-// nameInput.addEventListener("input", function(e){
-// e.target.setCustomValidity('');
-// });
-//}, false);
-//nameInput.addEventListener("keyup", function (event) {
-// if (nameInput.validity.valid) {
-// // show OK symbol:
-// nameInputIcon.className='el el-ok-sign icon-green';
-// } else {
-// // show ERROR symbol:
-// nameInputIcon.className='el el-error-alt icon-red';
-// }
-//}, false);
-//var checkPasswordValidity = function() {
-//
-// if (password1.value != password2.value) {
-// password1.setCustomValidity('Passwords must match.');
-// } else {
-// password1.setCustomValidity('');
-// }
-//};
-//password1.addEventListener('change', checkPasswordValidity, false);
-//password2.addEventListener('change', checkPasswordValidity, false);
+  
