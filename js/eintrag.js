656ea60890ad1ace5fc6820ba4ef54582b4f7fc3
Sven <flynn23@gmx.de>
2016-05-24 20:48:58 +0200

    Aufgabe 2 fertig
    woop woop

--- js/eintrag.js
+++ js/eintrag.js
@@ -10,25 +10,34 @@ nameInput.addEventListener("invalid", function (e) {
     if (nameInput.validity.valueMissing) {
         e.target.setCustomValidity("Bitte den Namen des Drehortes angeben!");
     } else if (!nameInput.validity.valid) {
-        e.target.setCustomValidity("Bitte nur Buchstaben angeben mit 2-30 Zeichen!");
+        e.target.setCustomValidity("Bitte nur Buchstaben angeben mit 4-30 Zeichen!");
     }
+    nameInput.addEventListener("keyup", function (e) {
+        e.target.setCustomValidity('');
+    });
 }, false);
 
 countryInput.addEventListener("invalid", function (e) {
     if (countryInput.validity.valueMissing) {
         e.target.setCustomValidity("Bitte den Namen des Ortes angeben!");
     } else if (!countryInput.validity.valid) {
-        e.target.setCustomValidity("Bitte nur Buchstaben angeben mit 2-30 Zeichen!");
+        e.target.setCustomValidity("Bitte nur Buchstaben angeben mit 4-30 Zeichen!");
     }
+    countryInput.addEventListener("keyup", function (e) {
+        e.target.setCustomValidity('');
+    });
 }, false);
 
 
-emailInput.addEventListener("invalid" , function (e) {
+emailInput.addEventListener("invalid", function (e) {
     if (emailInput.validity.valueMissing) {
         e.target.setCustomValidity("Bitte die Email-Adresse des Veranstalters angeben!");
-    } else if(emailInput.validity.patternMismatch) {
+    } else if (emailInput.validity.patternMismatch) {
         e.target.setCustomValidity("Die Email-Adresse muss mit @beuth-hochschule.de enden!");
     }
+    emailInput.addEventListener("keyup", function (e) {
+        e.target.setCustomValidity('');
+    });
 }, false);
 
 timefromInput.addEventListener("invalid", function (e) {
@@ -37,12 +46,18 @@ timefromInput.addEventListener("invalid", function (e) {
     } else if (!timefromInput.validity.valid) {
         e.target.setCustomValidity("Bitte korrekte Zeitangaben verwenden! (Bsp: 09:15)");
     }
+    timefromInput.addEventListener("keyup", function (e) {
+        e.target.setCustomValidity('');
+    });
 }, false);
 
-timetoInput.addEventListener("invalid" , function (e) {
+timetoInput.addEventListener("invalid", function (e) {
     if (timetoInput.validity.valueMissing) {
         e.target.setCustomValidity("Bitte die Uhrzeit der Veranstaltung angeben!");
     } else if (!timetoInput.validity.valid) {
         e.target.setCustomValidity("Bitte korrekte Zeitangaben verwenden! (Bsp: 09:15)");
     }
+    timetoInput.addEventListener("keyup", function (e) {
+        e.target.setCustomValidity('');
+    });
 }, false);
